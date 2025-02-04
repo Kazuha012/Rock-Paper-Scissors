9bab227c9c8c695551e388107abf9be0eaac74f2
Sachin Thapa <thapasachin766@gmail.com>
2025-02-04 21:08:16 +0545

    path modified

--- scripts/10-rock-paper-scissor.js
+++ scripts/10-rock-paper-scissor.js
@@ -60,8 +60,8 @@ function playGame(playerMove) {
 
     document.querySelector('.js-moves')
     .innerHTML = `You
-    <img src="images/${playerMove}-emoji.png"  class="move-icon">
-     <img src="images/${ComputerMove}-emoji.png" class="move-icon">
+    <img src="../images/${playerMove}-emoji.png"  class="move-icon">
+     <img src="../images/${ComputerMove}-emoji.png" class="move-icon">
     Computer`;
 }
 
